902612792c03c3c091a85800849d68baf7bb9f81
Brian Green <briangreenery@gmail.com>
2013-11-03 15:30:01 -0800

    Use handlebars & bootstrap

--- webserver/server.js
+++ webserver/server.js
@@ -1,9 +1,16 @@
 var express = require('express');
+var exphbs = require('express3-handlebars');
+var path = require('path');
 
 var app = express();
 
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+app.use(express.static(path.join(__dirname, 'deps')));
+
 app.get('/', function (req, res) {
-  res.send('hello, world!');
+  res.render('home');
 });
 
 app.listen(3000, '127.0.0.1');
