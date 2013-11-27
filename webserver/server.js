d646cff579d36a8c11493259b84fe46fbd52286a
Brian Green <briangreenery@gmail.com>
2013-11-27 10:17:47 -0800

    Disable x-powered-by, use compression

--- webserver/server.js
+++ webserver/server.js
@@ -5,16 +5,20 @@ var http = require('http');
 
 var app = express();
 
+app.disable('x-powered-by');
+
 app.engine('hbs', exphbs({defaultLayout: 'main.hbs'}));
 app.set('view engine', 'hbs');
 
+app.use(express.compress());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/evaluate', function (req, res) {
   http.get('http://ec2-204-236-174-221.us-west-1.compute.amazonaws.com?q=' + encodeURIComponent(req.query.q), function (upstreamRes) {
     upstreamRes.pipe(res);
   }).on('error', function (error) {
-    res.status(502).send(error.message);
+    res.status(502);
+    res.send(error.message);
   });
 });
 
@@ -23,6 +27,6 @@ app.get('/', function (req, res) {
 });
 
 var port = process.env.PORT || 3000;
-app.listen(port, '127.0.0.1');
+app.listen(port);
 
 console.log('Listening on port ' + port);
