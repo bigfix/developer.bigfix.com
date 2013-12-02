5d4d82481397246c11ae33df0179f2c4a789e2f2
Brian Green <briangreenery@gmail.com>
2013-12-01 23:55:17 -0800

    Don't use readFileSync

--- evaluator/server.js
+++ evaluator/server.js
@@ -12,12 +12,13 @@ app.get('/', function (req, res) {
   fs.writeFileSync(inFile, 'Q:' + req.query.q);
 
   child.spawn('C:\\qna\\eval.exe', [inFile, outFile, 'QnA']).on('close', function () {
-    var results = fs.readFileSync(outFile).toString().trim().split('\n');
-    for (var i = 0; i < results.length; ++i) {
-      results[i] = results[i].trim().substring(3);
-    }
-    res.send(JSON.stringify(results.slice(1)));
-  });
+    fs.readFile(outFile, function (err, data) {
+      var results = data.toString().trim().split('\n');
+      for (var i = 0; i < results.length; ++i) {
+        results[i] = results[i].trim().substring(3);
+      }
+      res.send(JSON.stringify(results.slice(1)));
+    });
 });
 
 app.listen(80);
