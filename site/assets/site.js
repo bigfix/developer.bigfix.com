025b5e4235e677b1c945b0c20243161c664f4034
Brian Green <briangreenery@gmail.com>
2015-03-07 21:37:25 -0800

    Use font-awesome for expand things

--- site/assets/site.js
+++ site/assets/site.js
@@ -1,3 +1,13 @@
 function toggleEntry(element) {
-  element.nextElementSibling.classList.toggle('entry-hidden');
+  var classList = element.firstElementChild.classList;
+
+  if (classList.contains('fa-plus-circle')) {
+    classList.remove('fa-plus-circle');
+    classList.add('fa-minus-circle');
+  } else {
+    classList.remove('fa-minus-circle');
+    classList.add('fa-plus-circle');
+  }
+
+  element.parentElement.nextElementSibling.classList.toggle('entry-hidden');
 }
