ce05a755f18898992888bccdb7b06aeb8dbff702
Brian Green <briangreenery@gmail.com>
2015-04-01 11:20:20 -0700

    Add support for relevance fenced code blocks

--- site/build/lib/renderText.js
+++ site/build/lib/renderText.js
@@ -4,13 +4,22 @@ var hljs = require('highlight.js'),
 
 hljs.registerLanguage('bigfix-relevance', require('hljs-bigfix-relevance'));
 
+/**
+ * Highlight relevance code.
+ */
+function highlightRelevance(relevance) {
+  return hljs.highlight('bigfix-relevance', relevance).value;
+}
+
+/**
+ * Parse the contents of an {{example}} block.
+ */
 function parseExample(text) {
   var example = { question: '', answers: [], errors: [] };
 
   text.trim().split('\n').forEach(function(line) {
     if (line.indexOf('Q:') === 0) {
-      example.question =
-        hljs.highlight('bigfix-relevance', line.substr(2).trim()).value;
+      example.question = highlightRelevance(line.substr(2).trim());
     } else if (line.indexOf('A:') === 0) {
       example.answers.push({text: line.substr(2).trim()});
     } else if (line.indexOf('E:') === 0) {
@@ -28,6 +37,9 @@ function escapeMarkdown(html) {
   return html.replace(/\*/g, '&#42;').replace(/_/g, '&#95;');
 }
 
+/**
+ * Render a markdown document to HTML.
+ */
 function renderText(text, templates) {
   var exampleData = {
     example: function() {
@@ -40,12 +52,25 @@ function renderText(text, templates) {
   var renderer = new marked.Renderer();
   var title = '';
 
+  // Override the 'heading' function so that we can automatically determine
+  // the title for this page based on the first heading.
+
   renderer.heading = function(text) {
     if (title === '') {
       title = text;
     }
 
     return marked.Renderer.prototype.heading.apply(this, arguments);
+  };
+
+  // Override the 'code' function so that we can colorize relevance.
+
+  renderer.code = function(code, language) {
+    if (language === 'relevance') {
+      return '<pre><code>' + highlightRelevance(code) + '</code></pre>';
+    }
+
+    return marked.Renderer.prototype.code.apply(this, arguments);
   };
 
   var content =
