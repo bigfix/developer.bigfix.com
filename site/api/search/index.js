6dbde857cd30f9b2b4803c21db9477b941f4452a
Brian Green <briangreenery@gmail.com>
2015-04-02 16:14:40 -0700

    Pages should be numbered from 1. Fixes #34.

--- site/api/search/index.js
+++ site/api/search/index.js
@@ -7,17 +7,17 @@ app.set('x-powered-by', false);
 
 app.get('/api/search', function(req, res) {
   var query = req.query.query;
-  var page = Number(req.query.page || 0);
+  var page = parseInt(req.query.page || '0', 10);
 
   if (!query || Number.isNaN(page)) {
     return res.status(400).end('invalid search query');
   }
 
-  if (page < 0) {
-    page = 0;
+  if (page < 1) {
+    page = 1;
   }
 
-  res.json(search(query, 25, page * 25));
+  res.json(search(query, 25, (page - 1) * 25));
 });
 
 app.listen(process.env.PORT);
