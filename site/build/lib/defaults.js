551780ac00cfd7757511cafa4de6ab5696e39fb0
baonguyen <baonguyen@us.ibm.com>
2016-04-18 17:06:08 -0700

    submenu in nav

--- site/build/lib/defaults.js
+++ site/build/lib/defaults.js
@@ -20,14 +20,49 @@ function getLocalDefaults(directory) {
  */
 function getDefaults(baseDir, currentDir) {
   var defaults = [];
-
+  var siteNav = getSiteNav(baseDir);
   while (currentDir !== baseDir) {
     defaults.push(getLocalDefaults(currentDir));
     currentDir = path.dirname(currentDir);
   }
 
   defaults.push(getLocalDefaults(baseDir));
+  defaults.push({
+    siteNav: siteNav
+  });
   return defaults.reverse().reduce(extend, {});
+}
+
+var siteNav = null;
+
+/**
+ * Aggregate all nav info from defaults.yml files to return the full nav tree.
+ */
+function getSiteNav(baseDir) {
+  if (siteNav !== null) {
+    return siteNav;
+  }
+  var baseConfig = getLocalDefaults(baseDir);
+  var root = baseConfig['rootNav'];
+  for (var i = 0; i < root.length; i++) {
+    var section = root[i];
+    if (section['folder']) {
+      if (section['href'] === undefined) {
+        section['href'] = '/' + section['folder'] + '/';
+      }
+      section['children'] = getNav(baseDir + '/' + section['folder']);
+    }
+  }
+  siteNav = root;
+  return siteNav;
+}
+
+function getNav(directory) {
+  var config = getLocalDefaults(directory);
+  if (config['nav'] === undefined) {
+    return [];
+  }
+  return config['nav'];
 }
 
 module.exports = getDefaults;
